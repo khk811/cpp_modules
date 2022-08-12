e3dcec3a25ac2ceceb795f24ce8714bd15f001ea
khk811 <hyunkyungkim811@gmail.com>
2022-08-12 17:27:22 +0900

    cpp_00 ex00 : working but need to revise

---
+++ cpp_00/ex00/megaphone.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+int	main(int argc, char **argv){
+	std::string	result;
+	if (argc >= 2){
+		for (int i = 1; i < argc; i++){
+			std::string	str(argv[i]);
+			for (size_t j = 0; j < str.length(); j++){
+				result += toupper(str[j]);
+			}
+		}
+	}
+	else{
+		result = "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
+	}
+	std::cout << result << std::endl;
+	return (0);
+}
