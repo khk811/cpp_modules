a7901ba4b508544f479c8f4dae9ea1da43bbe6fe
khk811 <hyunkyungkim811@gmail.com>
2022-08-19 21:28:26 +0900

    add basic method form

---
+++ cpp_00/ex02/Account.cpp
@@ -0,0 +1,31 @@
+#include "Account.hpp"
+
+Account::Account(int initial_deposit) {
+}
+
+Account::Account() {
+
+}
+
+Account::~Account() {
+}
+
+void	Account::makeDeposit(int deposit) {
+
+}
+
+bool	Account::makeWithdrawal(int withdrawl) {
+
+}
+
+int	Account::checkAmount() const {
+
+}
+
+void	Account::displayStatus() const {
+
+}
+
+void	Account::_displayTimestamp() {
+
+}
