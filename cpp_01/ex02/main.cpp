99370a3423432e353ebc686b99268c64daba54b9
khk811 <hyunkyungkim811@gmail.com>
2022-11-10 21:43:20 +0900

    주소값, 값을 모두 동일한 형태로 출력함

--- cpp_01/ex02/main.cpp
+++ cpp_01/ex02/main.cpp
@@ -6,11 +6,11 @@ int	main(void) {
 	std::string&	stringREF = the_statement;
 
 	std::cout << &(the_statement) << std::endl;
-	std::cout << stringPTR << std::endl;
+	std::cout << &(stringPTR) << std::endl;
 	std::cout << &(stringREF) << std::endl;
 
 	std::cout << the_statement << std::endl;
-	std::cout << *(stringPTR) << std::endl;
+	std::cout << stringPTR << std::endl;
 	std::cout << stringREF << std::endl;
 	return 0;
 }
