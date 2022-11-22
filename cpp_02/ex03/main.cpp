de68abf6bbd20aaf83dd64454a5a382ccd66b587
khk811 <hyunkyungkim811@gmail.com>
2022-11-22 21:46:47 +0900

    출력 메시지를 바꿈

--- cpp_02/ex03/main.cpp
+++ cpp_02/ex03/main.cpp
@@ -3,8 +3,8 @@
 int main( void ) {
 	bool	ret = bsp(Point(1, 1), Point(4, 1), Point(3, 7), Point(3, 1.007f));
 	if (ret == true)
-		std::cout << "true?" << std::endl;
+		std::cout << "True: the point is inside of triangle" << std::endl;
 	else
-		std::cout << "false goddammit!" << std::endl;
+		std::cout << "False: the point is outside of triangle" << std::endl;
 	return 0;
 }
