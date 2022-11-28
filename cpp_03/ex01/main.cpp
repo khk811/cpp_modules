0450dab16d302d67126ea36d700a0a24cb1bd1f1
khk811 <hyunkyungkim811@gmail.com>
2022-11-28 20:07:27 +0900

    main 함수 위 status 함수 추가

--- cpp_03/ex01/main.cpp
+++ cpp_03/ex01/main.cpp
@@ -1,16 +1,41 @@
 #include "ScavTrap.hpp"
 
+void	getScavTrapStatus(ScavTrap const& src) {
+
+	std::cout << "ScavTrap" << " [" << src.getClapTrapName() << "]";
+	std::cout << ": ";
+	std::cout << "HP => " << src.getClapTrapHP();
+	std::cout << " EP => " << src.getClapTrapEP();
+	std::cout << " AD => " << src.getClapTrapAD() << std::endl;
+}
+
+
+void	getClapTrapStatus(ClapTrap const& src) {
+
+	std::cout << "ClapTrap" << " [" << src.getClapTrapName() << "]";
+	std::cout << ": ";
+	std::cout << "HP => " << src.getClapTrapHP();
+	std::cout << " EP => " << src.getClapTrapEP();
+	std::cout << " AD => " << src.getClapTrapAD() << std::endl;
+}
+
 int	main(void) {
 	ClapTrap	steve("Steve");
 	ScavTrap	romeo("Romeo");
 
 	steve.attack(romeo.getClapTrapName());
 	romeo.takeDamage(steve.getClapTrapAD());
+	getClapTrapStatus(steve);
+	getScavTrapStatus(romeo);
 	steve.beRepaired(10);
 	romeo.beRepaired(20);
+	getClapTrapStatus(steve);
+	getScavTrapStatus(romeo);
 	romeo.takeDamage(15);
 	romeo.attack(steve.getClapTrapName());
 	steve.takeDamage(romeo.getClapTrapAD());
 	romeo.guardGate();
+	getClapTrapStatus(steve);
+	getScavTrapStatus(romeo);
 	return 0;
 }
