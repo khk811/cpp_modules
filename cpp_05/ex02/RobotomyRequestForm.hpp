d0b886efdab5af37ad84d4858e95eabe67b05ddc
khk811 <hyunkyungkim811@gmail.com>
2022-12-08 21:00:53 +0900

    Robotomy, Presidential 클래스 추가

---
+++ cpp_05/ex02/RobotomyRequestForm.hpp
@@ -0,0 +1,20 @@
+#ifndef ROMOTOMYREQUESTFORM_HPP
+#define ROMOTOMYREQUESTFORM_HPP
+
+#include "AForm.hpp"
+#include <cstdlib>
+
+class RobotomyRequestForm : public AForm
+{
+private:
+	std::string				target;
+	void					executeFormAction() const;
+public:
+	RobotomyRequestForm();
+	RobotomyRequestForm(std::string form_target);
+	RobotomyRequestForm(RobotomyRequestForm const& src);
+	~RobotomyRequestForm();
+	RobotomyRequestForm&	operator=(RobotomyRequestForm const& src);
+};
+
+#endif
--- cpp_05/ex02/main.cpp
+++ cpp_05/ex02/main.cpp
@@ -1,14 +1,20 @@
 #include "Bureaucrat.hpp"
 #include "AForm.hpp"
 #include "ShrubberyCreationForm.hpp"
+#include "RobotomyRequestForm.hpp"
+#include "PresidentialPardonForm.hpp"
 
 int	main(void) {
 	Bureaucrat	james("James", 100);
 	Bureaucrat	thomas("Thomas", 1);
+	Bureaucrat	riam("Riam", 2);
 	Bureaucrat	steve("Steve", 140);
 	// Bureaucrat	harry("Harry", 0);
 	Bureaucrat	romeo("Romeo", 75);
 	AForm*		shrubbery = new ShrubberyCreationForm("home");
+	AForm*		robot = new RobotomyRequestForm("delta");
+	AForm*		robot2 = new RobotomyRequestForm("gamma");
+	AForm*		wtf = new PresidentialPardonForm("Alexandra the mutant");
 
 	try {
 		james.increaseGrade();
@@ -20,6 +26,12 @@ int	main(void) {
 		// shrubbery->execute(steve);
 		romeo.signForm(*shrubbery);
 		shrubbery->execute(romeo);
+		thomas.signForm(*robot);
+		robot->execute(thomas);
+		riam.signForm(*robot2);
+		robot2->execute(riam);
+		thomas.signForm(*wtf);
+		wtf->execute(romeo);
 	} catch (std::exception& e) {
 		std::cout << "Error: " << e.what() << std::endl;
 		// delete shrubbery;
@@ -29,6 +41,9 @@ int	main(void) {
 	std::cout << steve << std::endl;
 	std::cout << romeo << std::endl;
 	delete shrubbery;
+	delete robot;
+	delete robot2;
+	std::cout << "\n===Check Memory Leak===" << std::endl;
 	system("leaks a.out");
 	return 0;
 }
