210adf58993fa9d15640e202076eda92dd02b444
khk811 <hyunkyungkim811@gmail.com>
2022-12-19 15:41:01 +0900

    출력에 개행 및 추가 문자열을 넣음, 생성 프로그램명 변경

--- cpp_06/ex00/main.cpp
+++ cpp_06/ex00/main.cpp
@@ -7,10 +7,13 @@ int	main(int argc, char** argv) {
 		try
 		{
 			ScalarConvert	convert(target);
+			std::cout << "\n\n===Convert Scalar===" << std::endl;
 			convert.printInputNType();
+			std::cout << "\n\n" << std::endl;
 		}
 		catch(const std::exception& e)
 		{
+			std::cout << "\n\n" << std::endl;
 			std::cout << e.what() << std::endl;
 		}
 		return 0;
