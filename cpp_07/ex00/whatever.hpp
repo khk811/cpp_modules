d461ef42e82d7b1d2e035836a6a7bd4f9eba145d
khk811 <hyunkyungkim811@gmail.com>
2022-12-20 19:03:47 +0900

    ex00: 일단 명세대로 작동은 하지만 template 선언을 변경해야 함

---
+++ cpp_07/ex00/whatever.hpp
@@ -0,0 +1,17 @@
+#ifndef WHATEVER_HPP
+#define WHATEVER_HPP
+
+// header 파일 내 템플릿 정의를 이렇게 하는지 모르겠음
+// 뭔가 다른 방법이 있을거 같은데 이게 맞나 싶음.햣
+
+template<typename T>
+void	swap(T& a, T& b);
+
+template<typename T>
+T&		min(T& a, T& b);
+
+template<typename T>
+T&		max(T& a, T& b);
+
+#endif
+
