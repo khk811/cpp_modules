45ddb6274844cf1bfc247f840b44052031fdb454
khk811 <hyunkyungkim811@gmail.com>
2022-12-22 20:52:01 +0900

    iter prototype 주석 추가, testcase를 위한 함수 작성

--- cpp_07/ex01/iter.hpp
+++ cpp_07/ex01/iter.hpp
@@ -3,6 +3,8 @@
 
 #include <iostream>
 
+// void	iter(T* addr, size_t len, F (*func)(T&));
+// void	iter(T* addr, size_t len, void (*func)(T&));
 template<typename T, typename F>
 void	iter(T* addr, size_t len, F func) {
 	for (size_t i = 0; i < len; i++)
@@ -11,11 +13,30 @@ void	iter(T* addr, size_t len, F func) {
 	}
 }
 
-// Function template for test;
+// Functions  for test;
 template<typename T>
-void	printElement(T a) {
+void	printElement(T& a) {
 	std::cout << a << std::endl;
 }
 
+template<typename T>
+int		printElementRetInt(T& b) {
+	std::cout << b << std::endl;
+	return 0;
+}
+
+int	plusOneToInt(int c) {
+	std::cout << c + 1 << std::endl;
+	return c + 1;
+}
+
+bool	eraseCharL(std::string& target) {
+	if (target.find('l') != std::string::npos) {
+		target = target.erase(target.find('l'), 1);
+		return true;
+	} else {
+		return false;
+	}
+}
 
 #endif
--- cpp_07/ex01/main.cpp
+++ cpp_07/ex01/main.cpp
@@ -4,5 +4,13 @@ int	main(void) {
 	int	int_arr[4] = {42, 21, 0, 100};
 
 	iter(int_arr, 4, printElement<int>);
+	iter(int_arr, 4, printElementRetInt<int>);
+	iter(int_arr, 4, plusOneToInt);
+	// iter(int_arr, 4, printSomething);
+
+	std::string	str_arr[3] = {"Hello", "World", "42Seoul"};
+	iter(str_arr, 3, printElement<std::string>);
+	iter(str_arr, 3, eraseCharL);
+	iter(str_arr, 3, printElement<std::string>);
 	return 0;
 }
