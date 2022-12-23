8a266b8e85009e775843092528e30c399f390168
khk811 <hyunkyungkim811@gmail.com>
2022-12-23 12:49:11 +0900

    iter 함수 프로토타입을 변경하고 overload를 함

--- cpp_07/ex01/iter.hpp
+++ cpp_07/ex01/iter.hpp
@@ -4,21 +4,29 @@
 #include <iostream>
 
 template<typename T, typename F>
-void	iter(T* addr, size_t len, F func) {
+void	iter(T* addr, size_t len, F (*func)(T&)) {
 	for (size_t i = 0; i < len; i++)
 	{
 		func(addr[i]);
 	}
 }
 
-// Functions  for test;
+template<typename T, typename F>
+void	iter(T* addr, size_t len, F (*func)(T const&)) {
+	for (size_t i = 0; i < len; i++)
+	{
+		func(addr[i]);
+	}
+}
+
+// Functions for test;
 template<typename T>
 void	printElement(T& a) {
 	std::cout << a << std::endl;
 }
 
 template<typename T>
-T*	printElementAddr(T& a) {
+T const*	printElementAddr(T const& a) {
 	std::cout << &a << std::endl;
 	return &a;
 }
