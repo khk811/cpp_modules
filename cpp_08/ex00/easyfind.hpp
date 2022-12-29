e32776a3163b7599ec58092be51c2c0a57aa581a
khk811 <hyunkyungkim811@gmail.com>
2022-12-29 16:58:55 +0900

    Template type이나 내부 변수 이름을 변경함

--- cpp_08/ex00/easyfind.hpp
+++ cpp_08/ex00/easyfind.hpp
@@ -9,21 +9,12 @@ class ItemCannotFound : public std::exception {
 	const char	*what() const throw();
 };
 
-template <typename Container>
-typename Container::iterator	easyfind(Container& ctain, int target) {
-	// find 함수를 써야 한다;; 와 미친...평가표에는 for문 금지!!
-	// 반복자를 쓴 매뉴얼 서치 금지!! 우와웅 ㅠㅠㅠㅠㅠ
-	// for (typename Container::iterator itr = ctain.begin(); itr !=  ctain.end(); itr++)
-	// {
-	// 	if (*(itr) == target) {
-	// 		return itr;
-	// 	}
-	// }
-	// throw ItemCannotFound();
-	typename Container::iterator	target_occur;
-	target_occur = std::find(ctain.begin(), ctain.end(), target);
-	if (target_occur != ctain.end()) {
-		return target_occur;
+template <typename T>
+typename T::iterator	easyfind(T& ctain, int target) {
+	typename T::iterator	find_result;
+	find_result = std::find(ctain.begin(), ctain.end(), target);
+	if (find_result != ctain.end()) {
+		return find_result;
 	} else {
 		throw ItemCannotFound();
 	}
