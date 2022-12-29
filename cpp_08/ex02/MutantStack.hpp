870188084af82a359c808bcfb237c7986520c4c0
khk811 <hyunkyungkim811@gmail.com>
2022-12-29 21:56:09 +0900

    const_반복자를 추가함

--- cpp_08/ex02/MutantStack.hpp
+++ cpp_08/ex02/MutantStack.hpp
@@ -8,16 +8,22 @@ template<typename T>
 class MutantStack : public std::stack<T>
 {
 public:
-	typedef typename std::stack<T>::container_type::iterator			iterator;
-	typedef typename std::stack<T>::container_type::reverse_iterator	reverse_iterator;
-	iterator			begin();
-	iterator			end();
-	reverse_iterator	rbegin();
-	reverse_iterator	rend();
+	typedef typename std::stack<T>::container_type::iterator				iterator;
+	typedef typename std::stack<T>::container_type::reverse_iterator		reverse_iterator;
+	typedef typename std::stack<T>::container_type::const_iterator			const_iterator;
+	typedef typename std::stack<T>::container_type::const_reverse_iterator	const_reverse_iterator;
+	iterator				begin();
+	iterator				end();
+	const_iterator			begin() const;
+	const_iterator			end() const;
+	reverse_iterator		rbegin();
+	reverse_iterator		rend();
+	const_reverse_iterator	rbegin() const;
+	const_reverse_iterator	rend() const;
 	MutantStack();
 	MutantStack(MutantStack const& src);
 	~MutantStack();
-	MutantStack&		operator=(MutantStack const& src);
+	MutantStack&			operator=(MutantStack const& src);
 };
 
 template<typename T>
@@ -31,12 +37,32 @@ typename MutantStack<T>::iterator	MutantStack<T>::end() {
 }
 
 template<typename T>
+typename MutantStack<T>::const_iterator	MutantStack<T>::begin() const {
+	return this->c.begin();
+}
+
+template<typename T>
+typename MutantStack<T>::const_iterator	MutantStack<T>::end() const {
+	return this->c.end();
+}
+
+template<typename T>
 typename MutantStack<T>::reverse_iterator	MutantStack<T>::rbegin() {
 	return this->c.rbegin();
 }
 
 template<typename T>
 typename MutantStack<T>::reverse_iterator	MutantStack<T>::rend() {
+	return this->c.rend();
+}
+
+template<typename T>
+typename MutantStack<T>::const_reverse_iterator	MutantStack<T>::rbegin() const {
+	return this->c.rbegin();
+}
+
+template<typename T>
+typename MutantStack<T>::const_reverse_iterator	MutantStack<T>::rend() const {
 	return this->c.rend();
 }
 
