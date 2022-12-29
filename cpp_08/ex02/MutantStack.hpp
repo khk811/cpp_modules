304f6c5555a743907c694b6492b8250637eda7b7
khk811 <hyunkyungkim811@gmail.com>
2022-12-29 16:12:10 +0900

    reverse_iterator 추가, main에 testcase 추가

--- cpp_08/ex02/MutantStack.hpp
+++ cpp_08/ex02/MutantStack.hpp
@@ -10,15 +10,16 @@ class MutantStack : public std::stack<T>
 public:
 	// typedef typename (c from stack<T>)::iterator	iterator;
 	// 이런식으로 정의가 되어야 할 것 같은데 도저히 못하겠음. syntax error 어쩔꺼야
-	typedef typename std::deque<T>::iterator	iterator;
-	iterator	begin();
-	iterator	end();
-	iterator	rbegin();
-	iterator	rend();
+	typedef typename std::deque<T>::iterator			iterator;
+	typedef typename std::deque<T>::reverse_iterator	reverse_iterator;
+	iterator			begin();
+	iterator			end();
+	reverse_iterator	rbegin();
+	reverse_iterator	rend();
 	MutantStack();
 	MutantStack(MutantStack const& src);
 	~MutantStack();
-	MutantStack&	operator=(MutantStack const& src);
+	MutantStack&		operator=(MutantStack const& src);
 };
 
 template<typename T>
@@ -32,12 +33,12 @@ typename MutantStack<T>::iterator	MutantStack<T>::end() {
 }
 
 template<typename T>
-typename MutantStack<T>::iterator	MutantStack<T>::rbegin() {
+typename MutantStack<T>::reverse_iterator	MutantStack<T>::rbegin() {
 	return this->c.rbegin();
 }
 
 template<typename T>
-typename MutantStack<T>::iterator	MutantStack<T>::rend() {
+typename MutantStack<T>::reverse_iterator	MutantStack<T>::rend() {
 	return this->c.rend();
 }
 
--- cpp_08/ex02/main.cpp
+++ cpp_08/ex02/main.cpp
@@ -1,30 +1,33 @@
 #include "MutantStack.hpp"
+#include <list>
 
-int	main(void) {
+void	subjectMutantStackTest(void) {
+	std::cout << "\n\n===Test from subject pdf : MutantStack===\n" << std::endl;
 	MutantStack<int> mstack;
 
+	std::cout << "\n=> push(5) to MutantStack" << std::endl;
 	mstack.push(5);
+	std::cout << "\n=> push(17) to MutantStack" << std::endl;
 	mstack.push(17);
 
+	std::cout << "\n\n---top element of MutantStack---\n" << std::endl;
 	std::cout << mstack.top() << std::endl;
 
+	std::cout << "\n=> pop() to MutantStack" << std::endl;
 	mstack.pop();
 
+	std::cout << "\n\n---Size of MutantStack after pop()---\n" << std::endl;
 	std::cout << mstack.size() << std::endl;
 
+	std::cout << "\n=> push(3) to MutantStack" << std::endl;
 	mstack.push(3);
+	std::cout << "\n=> push(5) to MutantStack" << std::endl;
 	mstack.push(5);
+	std::cout << "\n=> push(737) to MutantStack" << std::endl;
 	mstack.push(737);
 	//[...]
+	std::cout << "\n=> push(0) to MutantStack" << std::endl;
 	mstack.push(0);
-
-	std::cout << mstack.size() << std::endl;
-
-	MutantStack<int> copy_stack = mstack;
-
-	std::cout << copy_stack.size() << std::endl;
-	copy_stack.pop();
-	std::cout << copy_stack.top() << std::endl;
 
 	MutantStack<int>::iterator it = mstack.begin();
 	MutantStack<int>::iterator ite = mstack.end();
@@ -32,6 +35,7 @@ int	main(void) {
 	++it;
 	--it;
 
+	std::cout << "\n\n---print element from MutantStack---\n" << std::endl;
 	while (it != ite)
 	{
 		std::cout << *it << std::endl;
@@ -40,5 +44,58 @@ int	main(void) {
 
 	std::stack<int> s(mstack);
 
+	std::cout << "\n\n---Extra: reverse print element from MutantStack---\n" << std::endl;
+	for (MutantStack<int>::reverse_iterator it = mstack.rbegin(); it != mstack.rend() ; it++)
+	{
+		std::cout << *it << std::endl;
+	}
+}
+
+void	subjectListTest(void) {
+	std::cout << "\n\n===Test from subject pdf : std::list===\n" << std::endl;
+	std::list<int>	test_list;
+
+	std::cout << "\n=> push_back(5) to std::list" << std::endl;
+	test_list.push_back(5);
+	std::cout << "\n=> push_back(17) to std::list" << std::endl;
+	test_list.push_back(17);
+
+	std::cout << "\n\n---last(top) element from std::list---\n" << std::endl;
+	std::cout << test_list.back() << std::endl;
+
+	std::cout << "\n=> pop_back() to std::list" << std::endl;
+	test_list.pop_back();
+
+	std::cout << "\n\n---Size of std::list after pop_back()---\n" << std::endl;
+	std::cout << test_list.size() << std::endl;
+
+	std::cout << "\n=> push_back(3) to std::list" << std::endl;
+	test_list.push_back(3);
+	std::cout << "\n=> push_back(5) to std::list" << std::endl;
+	test_list.push_back(5);
+	std::cout << "\n=> push_back(737) to std::list" << std::endl;
+	test_list.push_back(737);
+	std::cout << "\n=> push_back(0) to std::list" << std::endl;
+	test_list.push_back(0);
+
+	std::list<int>::iterator	it = test_list.begin();
+	std::list<int>::iterator 	ite = test_list.end();
+
+	++it;
+	--it;
+
+	std::cout << "\n\n---print element from std::list---\n" << std::endl;
+	while (it != ite)
+	{
+		std::cout << *it << std::endl;
+		++it;
+	}
+}
+
+int	main(void) {
+	subjectMutantStackTest();
+	subjectListTest();
+	std::cout << "\n\n===Check Memory Leaks===\n" << std::endl;
+	system("leaks a.out");
 	return 0;
 }
