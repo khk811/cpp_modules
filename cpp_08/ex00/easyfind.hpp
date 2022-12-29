02ea777d91affff8dc0343021b21ebc041cb6ae7
khk811 <hyunkyungkim811@gmail.com>
2022-12-29 16:50:57 +0900

    thanks to jiwahn: find()를 써서 찾음

--- cpp_08/ex00/easyfind.hpp
+++ cpp_08/ex00/easyfind.hpp
@@ -2,6 +2,7 @@
 #define EASYFIND_HPP
 
 #include <iostream>
+#include <algorithm>
 #include <stdexcept>
 
 class ItemCannotFound : public std::exception {
@@ -10,13 +11,22 @@ class ItemCannotFound : public std::exception {
 
 template <typename Container>
 typename Container::iterator	easyfind(Container& ctain, int target) {
-	for (typename Container::iterator itr = ctain.begin(); itr !=  ctain.end(); itr++)
-	{
-		if (*(itr) == target) {
-			return itr;
-		}
+	// find 함수를 써야 한다;; 와 미친...평가표에는 for문 금지!!
+	// 반복자를 쓴 매뉴얼 서치 금지!! 우와웅 ㅠㅠㅠㅠㅠ
+	// for (typename Container::iterator itr = ctain.begin(); itr !=  ctain.end(); itr++)
+	// {
+	// 	if (*(itr) == target) {
+	// 		return itr;
+	// 	}
+	// }
+	// throw ItemCannotFound();
+	typename Container::iterator	target_occur;
+	target_occur = std::find(ctain.begin(), ctain.end(), target);
+	if (target_occur != ctain.end()) {
+		return target_occur;
+	} else {
+		throw ItemCannotFound();
 	}
-	throw ItemCannotFound();
 }
 
 #endif
